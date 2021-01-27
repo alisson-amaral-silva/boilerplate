42c868cc61cfd276a218897136b4592977f173f5
Alisson Alcântara Amaral da Silva - ASV <alisson.silva@iteris.com.br>
2021-01-27 20:18:57 -0300

    estilizando o componente Main e ajustando o teste do mesmo com uma atualizazção de snapshot

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)'],
-  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/']
 }
